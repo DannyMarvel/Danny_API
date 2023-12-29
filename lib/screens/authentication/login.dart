2c118c45d41046d5b271eca7e3b22dd7c94c57a0
DannyMarvel <dannymarvey@gmail.com>
2023-12-29 22:57:56 +0100

    new

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -84,6 +84,16 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                       Text(''),
                       Text(''),
+                      Text(''),
+                      Text(''),
+                      Text(''),
+      
+      
+      
+      
+                    
+                    
+                    
                     
                     GestureDetector(
                       onTap: () {
