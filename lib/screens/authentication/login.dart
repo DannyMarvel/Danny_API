ff3a1beb5c6849479213530e2e75e2eeb8ddff0e
DannyMarvel <dannymarvey@gmail.com>
2024-03-12 23:09:54 +0100

    fresh

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -85,6 +85,7 @@ class _LoginPageState extends State<LoginPage> {
                   Text(''),
                   Text(''),
                   Text(''),
+                  Text(''),
                 
               
                 
