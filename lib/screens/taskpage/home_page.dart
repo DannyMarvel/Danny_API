568131ba4c15d4970d301aaa509aca83ff6bd2f7
DannyMarvel <dannymarvey@gmail.com>
2024-06-04 23:51:30 +0100

    light

--- lib/screens/taskpage/home_page.dart
+++ lib/screens/taskpage/home_page.dart
@@ -87,7 +87,7 @@ class _HomePageState extends State<HomePage> {
               }
             }),
       ),
-      Text('Hello'),
+      
       floatingActionButton: FloatingActionButton(
         mini: true,
         onPressed: () {
